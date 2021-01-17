3c7b91a48aafcd69cbdda982d643591f67c987b0
David Sangojinmi <dsangojinmi26@gmail.com>
2021-01-17 15:54:34 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,25 @@ function App() {
           Learn React
         </a>
       </header>
+      <h3>Welcome to</h3>
+      <h1>Helping Hand!</h1>
+      <br></br>
+      <br></br>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+      <ol>
+          <li>Make an account as a helper or helpee.</li>
+          <li>
+              Helpers are those of you willing to perform specific tasks for helpees, that can range from simple tasks like a phone call,
+              to grocery shopping, or even just spending some time with them virtually.
+          </li>
+          <li>Helpees are those of you that require services due to being less physically able, or in need of help due to negative impacts from the Covid-19 pandemic.</li>
+          <li> Then, there is a system that matches helpers to helpees and allows people that can provide the right services to be matched to people looking for them.</li>
+      </ol>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button id="login" type="button" onClick="location.href='login.html'" class="btn btn-success">Login</button>
+      <button id="signup" type="button" onClick="location.href='signup.html'" class="btn btn-primary">Sign Up</button>
     </div>
   );
 }
